13aee220be949fde70b2e76fca8f6ee4edb4c64e
Lingyun Yang <lyyang@bnl.gov>
2015-02-02 20:09:58 -0500

    sel sp changed to char, general mergePvs

--- sofb2/sofbApp/src/aSubMergeSplit.c
+++ sofb2/sofbApp/src/aSubMergeSplit.c
@@ -10,19 +10,21 @@
 #include<waveformRecord.h>
 #include<epicsExport.h>
 #include<epicsTime.h>
+#include<epicsTypes.h>
 #include<math.h>
 #include<float.h>
 
 #define MERGE_INP(pval, dblk, val, sz)                  \
     if (dblk.type == CONSTANT) goto stats;              \
-    memcpy(pval, val, sz * sizeof(double));             \
+    memcpy(pval, val, sz);                              \
     pval += sz
 
 static long mergePvs(aSubRecord *pasub)
 {
     // output: waveform, avg, rms, min, max, std, var
-    double *pval = (double *)pasub->vala;
+    void *pval = pasub->vala;
     int i = 0;
+    int szi = 1;
     double s1 = 0.0;
     double s2 = 0.0;
     double xmin = DBL_MAX;
@@ -33,36 +35,64 @@ static long mergePvs(aSubRecord *pasub)
     memcpy(pval, pasub->a, pasub->noa * sizeof(double));
     pval += pasub->noa;
     */
-    MERGE_INP(pval, pasub->inpa, pasub->a, pasub->noa);
-    MERGE_INP(pval, pasub->inpb, pasub->b, pasub->nob);
-    MERGE_INP(pval, pasub->inpc, pasub->c, pasub->noc);
-    MERGE_INP(pval, pasub->inpd, pasub->d, pasub->nod);
-    MERGE_INP(pval, pasub->inpe, pasub->e, pasub->noe);
-    MERGE_INP(pval, pasub->inpf, pasub->f, pasub->nof);
-    MERGE_INP(pval, pasub->inpg, pasub->g, pasub->nog);
-    MERGE_INP(pval, pasub->inph, pasub->h, pasub->noh);
-    MERGE_INP(pval, pasub->inpi, pasub->i, pasub->noi);
-    MERGE_INP(pval, pasub->inpj, pasub->j, pasub->noj);
-    MERGE_INP(pval, pasub->inpk, pasub->k, pasub->nok);
-    MERGE_INP(pval, pasub->inpl, pasub->l, pasub->nol);
-    MERGE_INP(pval, pasub->inpm, pasub->m, pasub->nom);
-    MERGE_INP(pval, pasub->inpn, pasub->n, pasub->non);
-    MERGE_INP(pval, pasub->inpo, pasub->o, pasub->noo);
-    MERGE_INP(pval, pasub->inpp, pasub->p, pasub->nop);
-    MERGE_INP(pval, pasub->inpq, pasub->q, pasub->noq);
-    MERGE_INP(pval, pasub->inpr, pasub->r, pasub->nor);
-    MERGE_INP(pval, pasub->inps, pasub->s, pasub->nos);
-    MERGE_INP(pval, pasub->inpt, pasub->t, pasub->not);
-    MERGE_INP(pval, pasub->inpu, pasub->u, pasub->nou);
+    if (pasub->inpa.type == CONSTANT)  goto finish;
+    /* kind of hard coded */
+    switch(pasub->ftva) {
+    case epicsInt8T:
+    case epicsUInt8T: szi = sizeof(epicsInt8); break;
+    case epicsFloat64T: szi = sizeof(epicsFloat64); break;
+    case epicsFloat32T: szi = sizeof(epicsFloat32); break;
+    default:
+        fprintf(stderr, "not supported: %d\n", pasub->ftva);
+    }
+        
+    MERGE_INP(pval, pasub->inpa, pasub->a, pasub->noa*szi);
+    /* #ifdef epicsTypesGLOBAL */
+    /* fprintf(stderr, "FTVA: %d, %d, %s\n", pasub->ftva, epicsFloat64T, epicsTypeCodeNames[pasub->ftva]); */
+    /* fprintf(stderr, "FTVA: %d\n", epicsTypeSizes[0]); */
+    /* #endif */
+    MERGE_INP(pval, pasub->inpb, pasub->b, pasub->nob*szi);
+    MERGE_INP(pval, pasub->inpc, pasub->c, pasub->noc*szi);
+    MERGE_INP(pval, pasub->inpd, pasub->d, pasub->nod*szi);
+    MERGE_INP(pval, pasub->inpe, pasub->e, pasub->noe*szi);
+    MERGE_INP(pval, pasub->inpf, pasub->f, pasub->nof*szi);
+    MERGE_INP(pval, pasub->inpg, pasub->g, pasub->nog*szi);
+    MERGE_INP(pval, pasub->inph, pasub->h, pasub->noh*szi);
+    MERGE_INP(pval, pasub->inpi, pasub->i, pasub->noi*szi);
+    MERGE_INP(pval, pasub->inpj, pasub->j, pasub->noj*szi);
+    MERGE_INP(pval, pasub->inpk, pasub->k, pasub->nok*szi);
+    MERGE_INP(pval, pasub->inpl, pasub->l, pasub->nol*szi);
+    MERGE_INP(pval, pasub->inpm, pasub->m, pasub->nom*szi);
+    MERGE_INP(pval, pasub->inpn, pasub->n, pasub->non*szi);
+    MERGE_INP(pval, pasub->inpo, pasub->o, pasub->noo*szi);
+    MERGE_INP(pval, pasub->inpp, pasub->p, pasub->nop*szi);
+    MERGE_INP(pval, pasub->inpq, pasub->q, pasub->noq*szi);
+    MERGE_INP(pval, pasub->inpr, pasub->r, pasub->nor*szi);
+    MERGE_INP(pval, pasub->inps, pasub->s, pasub->nos*szi);
+    MERGE_INP(pval, pasub->inpt, pasub->t, pasub->not*szi);
+    MERGE_INP(pval, pasub->inpu, pasub->u, pasub->nou*szi);
 
 stats:
-    pval = (double*) pasub->vala;
-    for (i = 0; i < pasub->nova; ++i) {
-        s1 += pval[i];
-        s2 += pval[i] * pval[i];
-        if (pval[i] < xmin) xmin = pval[i];
-        if (pval[i] > xmax) xmax = pval[i];
+    if (pasub->ftva == epicsFloat64T) {
+        double *pdval = (double*) pasub->vala;
+        for (i = 0; i < pasub->nova; ++i) {
+            s1 += pdval[i];
+            s2 += pdval[i] * pdval[i];
+            if (pdval[i] < xmin) xmin = pdval[i];
+            if (pdval[i] > xmax) xmax = pdval[i];
+        }
+    } else if (pasub->ftva == epicsFloat32T) {
+        float *pfval = (float*) pasub->vala;
+        for (i = 0; i < pasub->nova; ++i) {
+            s1 += pfval[i];
+            s2 += pfval[i] * pfval[i];
+            if (pfval[i] < xmin) xmin = pfval[i];
+            if (pfval[i] > xmax) xmax = pfval[i];
+        }
+    } else {
+        goto finish;
     }
+        
     s2 /= pasub->nova;
     s1 /= pasub->nova;
     xrms = sqrt(s2);
